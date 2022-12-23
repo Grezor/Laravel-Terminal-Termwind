7d4ef90c90a490ef31d06389d8c28b2322087d80
Geoffrey Duplessi <grezor.gamer@gmail.com>
2022-12-23 20:08:28 +0100

    Generate fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::factory(10)->create();
+        \App\Models\Blog::factory(10)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
